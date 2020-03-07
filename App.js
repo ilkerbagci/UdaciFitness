2426b90ae33dfe7f9497da50788534fc13970b48
İlker Bağcı <ilkerbagci0@gmail.com>
2020-03-07 12:19:57 +0300

    Style the StatusBar
    Restyle the classes

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import  reducer from './reducers';
-import TabNav from './components/TabNavigator';
+import React from 'react'
+import { View, Platform, StatusBar } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import TabNav from './components/TabNavigator'
+import Constants from 'expo-constants'
+import { purple } from './utils/colors'
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+    return (
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
+}
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={createStore( reducer)}>
-        <View style={{ flex: 1}}>
-          <TabNav />
-        </View>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={createStore(reducer)}>
+                <View style={{ flex: 1 }}>
+                    <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+                    <TabNav />
+                </View>
+            </Provider>
+        )
+    }
 }
