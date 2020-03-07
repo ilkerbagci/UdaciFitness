7f15b55dbcf8d9b06cd58dec8031aceaaec0c9f2
İlker Bağcı <ilkerbagci0@gmail.com>
2020-03-07 12:02:39 +0300

    Add TabNavigator

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import History from './components/History'
+import React from 'react';
+import { View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import  reducer from './reducers';
+import TabNav from './components/TabNavigator';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <Provider store={createStore(reducer)}>
-                <View style={{ flex: 1 }}>
-                    <History />
-                </View>
-            </Provider>
-        )
-    }
-
+  render() {
+    return (
+      <Provider store={createStore( reducer)}>
+        <View style={{ flex: 1}}>
+          <TabNav />
+        </View>
+      </Provider>
+    );
+  }
 }
