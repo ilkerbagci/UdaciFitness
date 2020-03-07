0a9792a067d7da87dd7fd1625b4e2944ee41df77
İlker Bağcı <ilkerbagci0@gmail.com>
2020-03-07 14:32:27 +0300

    Some structural changes

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Platform, StatusBar } from 'react-native'
+import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import TabNav from './components/TabNavigator'
+import History from './components/History'
+import { createAppContainer, createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation'
+import { white, purple } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import Constants from 'expo-constants'
-import { purple } from './utils/colors'
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
     return (
@@ -14,6 +17,42 @@ function UdaciStatusBar({ backgroundColor, ...props }) {
         </View>
     )
 }
+
+const Tabs = {
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+        },
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+        },
+    },
+}
+
+const navigationOptions = {
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : purple,
+            shadowColor: 'rgba(0, 0, 0, 0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+}
+
+const TabNav = createAppContainer(Platform.OS === 'ios' ? createBottomTabNavigator(Tabs, navigationOptions) : createMaterialTopTabNavigator(Tabs, navigationOptions))
 
 export default class App extends React.Component {
     render() {
