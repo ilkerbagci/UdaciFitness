b39274073e10863d4da4376052eddc92b5ea74a8
İlker Bağcı <ilkerbagci0@gmail.com>
2020-03-08 02:34:31 +0300

    Add location permission request

--- components/Live.js
+++ components/Live.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import { calculateDirection } from '../utils/helpers'
+import * as Permissions from 'expo-permissions'
+import * as Location from 'expo-location'
 
 export default class Live extends Component {
     state = {
@@ -11,7 +14,46 @@ export default class Live extends Component {
         direction: ''
     }
     askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted')
+                    return this.setLocation()
 
+                this.setState({ status })
+            })
+            .catch(error => console.warn('error asking Location permission: ', error))
+    }
+
+    componentDidMount() {
+        Permissions.getAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                }
+
+                this.setState({ status })
+            })
+            .catch(error => {
+                console.warn("Errorgetting location permission: ", error)
+                this.setState({ status: 'undetermined' })
+            })
+    }
+
+    setLocation = () => {
+        Location.watchPositionAsync({
+            enableHighAccuracy: true,
+            timeInterval: 1,
+            distanceInterval: 1
+        }, ({ coords }) => {
+            const newDirection = calculateDirection(coords.heading)
+            const { direction } = this.state
+
+            this.setState({
+                coords,
+                status: 'granted',
+                direction: newDirection
+            })
+        })
     }
 
     render() {
