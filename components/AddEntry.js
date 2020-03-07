eae129c385bb76cabcf7d2c5bdbcff67b17c5b07
İlker Bağcı <ilkerbagci0@gmail.com>
2020-03-07 15:13:40 +0300

    Add goBack action into AddEntry and reset buttons

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,6 +14,7 @@ import { removeEntry, submitEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from "../utils/colors";
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn({ onPress }) {
     return (
@@ -76,7 +77,7 @@ class AddEntry extends Component {
 
         this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
-        // Navigate to home
+        this.toHome()
 
         submitEntry({ key, entry });
 
@@ -90,10 +91,17 @@ class AddEntry extends Component {
             [key]: getDailyReminderValue()
         }))
 
-        // Route to Home
-
+        this.props.dispatch(addEntry({ [key]: getDetailsReminderValue() }))
+        this.toHome()
         removeEntry(key)
     }
+    
+    toHome = () => {
+        this.props.navigation.dispatch(NavigationActions.back({
+            key: 'AddEntry'
+        }))
+    }
+
 
     render() {
 
